a5f6d37cce9cd6043c2bec731c3edf4a0cf70613
Philipp von Weitershausen <philipp@weitershausen.de>
2009-12-09 22:32:26 +0000

    Project skeleton as it came out of the add-on builder
    (see https://addons.mozilla.org/en-US/developers/tools/builder)

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.PrefsNotTools.boolpref", false);
+pref("extensions.PrefsNotTools.intpref", 0);
+pref("extensions.PrefsNotTools.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.prefsnottools@philikon.de.description", "chrome://PrefsNotTools/locale/overlay.properties");
