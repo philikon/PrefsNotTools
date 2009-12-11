8f2e312014f505d4b0959807c49215ba9162784e
Philipp von Weitershausen <philipp@weitershausen.de>
2009-12-11 01:38:10 +0000

    This extension needs no preferences of its own

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,2 @@
-pref("extensions.PrefsNotTools.boolpref", false);
-pref("extensions.PrefsNotTools.intpref", 0);
-pref("extensions.PrefsNotTools.stringpref", "A string");
-
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.prefsnottools@philikon.de.description", "chrome://PrefsNotTools/locale/overlay.properties");
